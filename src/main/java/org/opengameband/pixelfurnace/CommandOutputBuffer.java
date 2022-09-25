c47f2643ea6a4d8d4b3a10ed9e5c15423362a2da
Zaprit <henry@vorax.org>
2022-09-25 19:25:31 +0100

    Fixed build and added start to HID

---
+++ src/main/java/org/opengameband/pixelfurnace/CommandOutputBuffer.java
@@ -0,0 +1,12 @@
+//package org.opengameband.pixelfurnace;
+//
+//import java.io.Serializable;
+//
+///**
+// * @author Zaprit <henry@vorax.org>
+// */
+//public class CommandOutputBuffer {
+//    short timezone1;
+//    short timezone2;
+//    short timezone2;
+//}
---
+++ src/main/java/org/opengameband/pixelfurnace/GamebandHID.java
@@ -0,0 +1,12 @@
+package org.opengameband.pixelfurnace;
+
+import org.hid4java.HidDevice;
+import org.hid4java.HidManager;
+
+/**
+ * @author Zaprit <henry@vorax.org>
+ */
+public class GamebandHID {
+
+
+}
